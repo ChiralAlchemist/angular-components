abde7fd6fb953cdd26a54c4d9c9f0fae04eccaab
Joseph Ashby <jashby473@gmail.com>
2017-02-08 18:09:09 -0800

    completed step 3

--- client/app/components/blog/blog.controller.js
+++ client/app/components/blog/blog.controller.js
@@ -1,3 +1,18 @@
 // TODO: create the blog controller
 // check blog.html for hints on what the controller
 // should have/do
+import _ from 'lodash';
+
+class BlogController {
+  constructor() {
+    this.message = "hello";
+    this.posts = _.times(9, i => {
+      return {
+        author : 'a cat',
+        title: 'hello'
+      }
+    })
+  }
+}
+
+export {BlogController}
